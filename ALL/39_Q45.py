9fe7b3746a1934a513d37a4756741391ae3069b0
Dhruv-Trent <dhruvkanubhaipatel@trentu.ca>
2025-08-11 10:58:54 -0400

    just made changes in folder structure

--- ALL/39_Q45.py
+++ ALL/39_Q45.py
@@ -5,4 +5,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = []
+    res = sol.jump(nums)
+    print(res)
