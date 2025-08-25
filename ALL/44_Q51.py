462d5ef460f94a24ce76225345dbfdaff51c11ec
Dhruv-Trent <dhruvkanubhaipatel@trentu.ca>
2025-08-24 21:45:36 -0400

    adding more

---
+++ ALL/44_Q51.py
@@ -0,0 +1,20 @@
+# Problem:- 51. N-Queens
+
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        
+
+
+        
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    n = 4
+    res = sol.solveNQueens(n)
+    print(res)
+    
