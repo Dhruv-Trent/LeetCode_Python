320f52f070c1e2828d4b1430c7b230186244673d
Dhruv-Trent <dhruvkanubhaipatel@trentu.ca>
2025-12-18 14:03:39 -0500

    all done with text justification code it was tough in first but when u see how to solve it works.

--- ALL/53_Q68.py
+++ ALL/53_Q68.py
@@ -8,42 +8,37 @@ class Solution(object):
         :rtype: List[str]
         """
         res = []
+        line = []
+        length = 0
         i = 0
-        n = len(words)
         
-        while i < n:
-            # Determine which words fit on this line
-            line_len = len(words[i])
-            j = i + 1
-            while j < n and line_len + 1 + len(words[j]) <= maxWidth:
-                line_len += 1 + len(words[j])
-                j += 1
-
-            # number of words in this line
-            num_words = j - i
-            line = ""
-            
-            # If last line or line has only one word, left-justify
-            if j == n or num_words == 1:
-                line = " ".join(words[i:j])
-                line += " " * (maxWidth - len(line))
-            else:
-                # fully justify
-                total_spaces = maxWidth - sum(len(words[k]) for k in range(i, j))
-                space_between_words = total_spaces // (num_words - 1)
-                extra_spaces = total_spaces % (num_words - 1)
+        while i<len(words):
+            if length + len(line) + len(words[i])>maxWidth:
+                extraSpaces = maxWidth - length
+                spaces = extraSpaces// max(1,len(line)-1)
+                remainder = extraSpaces % max(1,len(line)-1)
                 
-                for k in range(i, j - 1):
-                    line += words[k]
-                    line += " " * (space_between_words + (1 if k - i < extra_spaces else 0))
-                line += words[j - 1]
+                for j in range(max(1,len(line)-1)):
+                    line[j] += ' ' * spaces
+                    if remainder:
+                        line[j]+=" "
+                        remainder-=1
+                res.append("".join(line))
+                
+                line = []
+                length = 0
+                
+            line.append(words[i])
+            length+= len(words[i])
+            i+=1
             
-            res.append(line)
-            i = j
-        
+        lastLine = " ".join(line)
+        trailSpaces = maxWidth - len(lastLine)
+        res.append(lastLine+" "*trailSpaces)
+                
+            
+            
         return res
-        
-                  
         
 
 if __name__ == '__main__':
