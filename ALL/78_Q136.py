36a0e1df33154aad7e0c33a31016fe6a90c56bc2
Dhruv-Trent <dhruvkanubhaipatel@trentu.ca>
2026-01-19 16:25:18 -0500

    Q-136 single Number too easy to solve just took 2 mins to solve this.

---
+++ ALL/78_Q136.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for n in nums:
+            res = n^res
+        return res
+        
+        
+if __name__ == '__main__':
+    sol = Solution()
+    # nums = [2,2,1]
+    nums = [1,2,2,4,1,3,4,3,9,8,5,8,5] 
+    res = sol.singleNumber(nums)
+    print(res)
+            
